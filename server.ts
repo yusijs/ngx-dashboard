fdb6e37ccd4a2728ef44b83c84fc93f97cf39167
Gogs <gogs@fake.local>
2016-12-28 22:57:47 +0100

    Add sockets to server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,20 @@
 import express = require('express');
 import sockets = require('socket.io');
+import http = require('http');
+import Socket from './sockets';
+
 
 const app = express();
+const server = http.createServer(app);
+
+// Use Angular CLI's output dir for static files
+app.use(express.static('charts/dist'));
+
+
+// Start express server on port 3000
+server.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
+
+// Instantiate websockets
+const io = new Socket(server);
